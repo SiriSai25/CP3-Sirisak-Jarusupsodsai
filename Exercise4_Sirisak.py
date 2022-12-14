4d4d8c3aa782be9baa0a067118ae7a86c872657c
SiriSai25 <2548sirisak@gmail.com>
2022-12-14 12:39:05 +0700

    Frist Commit
    Added Exercise4

---
+++ Exercise4_Sirisak.py
@@ -0,0 +1,11 @@
+fe = (78)
+gb = (81)
+ics = (75)
+cp = (87)
+
+print("--- Your Score ---")
+
+print("Foundation English :", fe)
+print("General Business :", gb)
+print("Introduction to Computer Systems :", ics)
+print("Computer Programming :", cp)
